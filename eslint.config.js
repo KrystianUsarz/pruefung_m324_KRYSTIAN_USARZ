baab24c75edb4fdd9714d8dc8bf533701876d937
Krystian  Usarz <usarz.krystian7@gmail.com>
2024-12-12 09:37:14 +0100

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'prefer-const': 'error',
+      'eqeqeq': 'error'
+    }
+  },
+  pluginJs.configs.recommended,
+];
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
+
+let myUnusedVariable = 0; // This should trigger both 'no-unused-vars' and 'prefer-const' errors
+var myUnused = 10;
+if (myUnused == 10) { // This should trigger the 'eqeqeq' error
+  console.log('This is a test.');
+}
