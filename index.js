1d72d1ed27694c944b47f82e79d1cd083efa7ecb
Krystian  Usarz <usarz.krystian7@gmail.com>
2024-12-12 09:38:35 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
 
-let myUnusedVariable = 0; // This should trigger both 'no-unused-vars' and 'prefer-const' errors
-var myUnused = 10;
-if (myUnused == 10) { // This should trigger the 'eqeqeq' error
+const myUnusedVariable = 0; // This should trigger both 'no-unused-vars' and 'prefer-const' errors
+
+if (myUnusedVariable === 0) { // This should trigger the 'eqeqeq' error
   console.log('This is a test.');
 }
