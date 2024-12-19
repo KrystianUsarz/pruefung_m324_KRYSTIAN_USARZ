e3b89563c876bc9352aedd070631fc294ff6db3a
Krystian  Usarz <usarz.krystian7@gmail.com>
2024-12-19 10:10:11 +0100

    create merge conflicht from pipeline

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ console.log(chalk.blue('Hello world!'));
 
 const myUnusedVariable = 0; // This should trigger both 'no-unused-vars' and 'prefer-const' errors
 
-if (myUnusedVariable === 0) { // This should trigger the 'eqeqeq' error
+if (myUnusedVariable == 0) { // This should trigger the 'eqeqeq' error
   console.log('This is a test.');
 }
