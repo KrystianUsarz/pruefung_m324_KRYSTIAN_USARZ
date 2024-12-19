96fd76b63dad6ac692eb3482fc910efa0e17e453
Krystian  Usarz <usarz.krystian7@gmail.com>
2024-12-19 10:56:03 +0100

    fix formatting issues

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
-console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'))
 
-var myUnusedVariable = 0;
+var myUnusedVariable = 0
 
 if (myUnusedVariable === 0) {
-  console.log('This is a test.');
+  console.log('This is a test.')
 }
