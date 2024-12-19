68e61f680f622c8b85f5eafcd2c19f06b289d93f
Krystian  Usarz <usarz.krystian7@gmail.com>
2024-12-19 10:37:37 +0100

    download act and test with errors

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
 
-const myUnusedVariable = 0; // This should trigger both 'no-unused-vars' and 'prefer-const' errors
+var myUnusedVariable = 0;
+var unused;
 
-if (myUnusedVariable == 0) { // This should trigger the 'eqeqeq' error
+if (myUnusedVariable === 0) {
   console.log('This is a test.');
 }
